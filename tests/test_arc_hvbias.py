1c81173ec6c4bc8b0f864abdb2649d13c977335b
Giles Knap <giles.knap@diamond.ac.uk>
2022-03-22 13:28:23 +0000

    remove debugging test

--- tests/test_arc_hvbias.py
+++ tests/test_arc_hvbias.py
@@ -5,12 +5,11 @@ import cothread
 
 from arc_hvbias import __main__, __version__
 
-
-def test_execution_debug():
-    cmd = [sys.executable, "-m", "arc_hvbias"]
-    result = subprocess.check_output(cmd)
-    cothread.Sleep(1000)
-    print(result.decode())
+# def test_execution_debug():
+#     cmd = [sys.executable, "-m", "arc_hvbias"]
+#     result = subprocess.check_output(cmd)
+#     cothread.Sleep(1000)
+#     print(result.decode())
 
 
 def test_cli_version():
